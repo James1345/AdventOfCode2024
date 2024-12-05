f5265b9c94844aea93b3309cfe454f1a3c0281a8
James McMahon <james1345@googlemail.com>
2024-12-05 13:49:01 +0000

    day 3 part 2

--- AOC2024/day3/Day3.cs
+++ AOC2024/day3/Day3.cs
@@ -8,11 +8,33 @@ public abstract partial class Day3
     {
         var input = File.ReadAllText("day3/input.txt");
         
+        var total = ExecuteMuls(input);
+
+        Console.WriteLine(total);
+        
+
+        var chunks = input.Split("don't()").ToList();
+        
+        List<string> enabledChunks = [chunks.First()];
+        chunks.RemoveAt(0);
+
+        foreach (var chunk in chunks.ToList())
+        {
+            var subChunks = chunk.Split("do()").ToList();
+            subChunks.RemoveAt(0);
+            enabledChunks.AddRange(subChunks);
+        }
+        
+        Console.WriteLine(enabledChunks.Sum(ExecuteMuls));
+
+    }
+
+    private static int ExecuteMuls(string input)
+    {
         var matchCollection = MulRegex().Matches(input);
 
         var total = matchCollection.Sum(match => int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
-        
-        Console.WriteLine(total);
+        return total;
     }
 
     [GeneratedRegex(@"mul\((\d+),(\d+)\)")]
