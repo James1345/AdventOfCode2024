8bf971e905cfb2557983868cfcfeffef9d0614c7
James McMahon <james1345@googlemail.com>
2024-12-06 13:32:39 +0000

    Day 6 part 2

--- AOC2024/day6/Day6.cs
+++ AOC2024/day6/Day6.cs
@@ -17,27 +17,71 @@ public abstract class Day6
         }
         
         Console.WriteLine(visited.Count);
+        
+        CheckLoops(input);
+    }
+
+    private static void CheckLoops(string[] input)
+    {
+        var count = 0;
+        for (var y = 0; y < input.Length; y++)
+        {
+            for (var x = 0; x < input[y].Length; x++)
+            {
+                var (obstacles, guard, size) = Parse(input);
+                if (guard.Location.X == x && guard.Location.Y == y)
+                {
+                    continue;
+                }
+                obstacles.Add(new Point2D(x, y));
+                if (HasLoop(guard, size, obstacles))
+                {
+                    count++;
+                }
+            }
+        }
+        
+        Console.WriteLine(count);
+        
+    }
+
+    private static bool HasLoop(Guard guard, Size size, HashSet<Point2D> obstacles)
+    {
+        var visited = new HashSet<(Point2D, Direction)>();
+
+        while (guard.Location is { X: >= 0, Y: >= 0 } && guard.Location.X < size.Width && guard.Location.Y < size.Height)
+        {
+            visited.Add((guard.Location, guard.Facing));
+            guard.Move(obstacles);
+            
+            if (visited.Contains((guard.Location, guard.Facing)))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 
     private class Guard(Point2D location, Direction facing)
     {
         public Point2D Location = location;
-        private Direction _facing = facing;
+        public Direction Facing = facing;
 
         public void Move(HashSet<Point2D> obstacles)
         {
-            while (obstacles.Contains(Location.Next(_facing)))
+            while (obstacles.Contains(Location.Next(Facing)))
             {
                 Rotate();
             }
 
-            Location = Location.Next(_facing);
+            Location = Location.Next(Facing);
         }
 
         private void Rotate()
         {
-            _facing = (Direction)(((int)_facing + 1) % 4);
+            Facing = (Direction)(((int)Facing + 1) % 4);
         }
     }
 
