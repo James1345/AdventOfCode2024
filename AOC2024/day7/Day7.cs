4d357c3b85c333a25ea1d4b8f1a7f9e6e67f5fac
James McMahon <james1345@googlemail.com>
2024-12-07 13:35:24 +0000

    day 7 part 2

--- AOC2024/day7/Day7.cs
+++ AOC2024/day7/Day7.cs
@@ -13,6 +13,10 @@ public class Day7
 
         var total = eqns.Where(it => it.Test()).Sum(it => it.TestValue);
         Console.WriteLine(total);
+        
+        total = eqns.Where(it => it.Test2()).Sum(it => it.TestValue);
+        Console.WriteLine(total);
+        
     }
 
     private record Eqn(long TestValue, List<int> Values)
@@ -47,8 +51,48 @@ public class Day7
             return false;
         }
         
+        public bool Test2()
+        {
+            var intervals = Values.Count - 1;
+            for (int operatorPattern = 0; operatorPattern < Math.Pow(3, intervals); operatorPattern++)
+            {
+                var operatorQueue = ToBase(operatorPattern, 3).PadLeft(intervals, '0');
+                var valueQueue = new Queue<int>(Values);
+                
+                long acc = valueQueue.Dequeue();
+                var j = 0;
+                while (valueQueue.Count != 0)
+                {
+                    switch (operatorQueue[j])
+                    {
+                        case '0': acc *= valueQueue.Dequeue(); break;
+                        case '1': acc += valueQueue.Dequeue(); break;
+                        case '2': acc = Convert.ToInt64($"{acc}{valueQueue.Dequeue()}"); break;
+                    }
+                    j++;
+                }
+
+                if (acc == TestValue) return true;
+                
+            }
+            return false;
+        }
+
+        private static string ToBase(int value, short baseValue)
+        {
+            var digits = new List<string>();
+            while (value > 0)
+            {
+                digits.Add($"{value % baseValue}");
+                value /= baseValue;
+            }
+            digits.Reverse();
+            return string.Join("", digits);
+        }
+        
         
     }
+    
     
     
 }
