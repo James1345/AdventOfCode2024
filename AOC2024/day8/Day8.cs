e7eae41491700cc87675cd3eb74fa67062d98f85
James McMahon <james1345@googlemail.com>
2024-12-08 11:34:08 +0000

    Day 8 part 2

--- AOC2024/day8/Day8.cs
+++ AOC2024/day8/Day8.cs
@@ -5,9 +5,17 @@ public abstract class Day8
     public static void Invoke()
     {
         var input = File.ReadAllLines("day8/input.txt");
-        
+
         var size = new Point2D(input[0].Length, input.Length);
-        
+
+        var antennae = ParseMap(input);
+
+        CalculateAntinodeCount(antennae, size);
+        CalculateResonantAntinodeCount(antennae, size);
+    }
+
+    private static Dictionary<char, List<Point2D>> ParseMap(string[] input)
+    {
         var antennae = new Dictionary<char, List<Point2D>>();
 
         for (int y = 0; y < input.Length; y++)
@@ -20,11 +28,17 @@ public abstract class Day8
                     {
                         antennae.Add(input[y][x], new List<Point2D>());
                     }
+
                     antennae[input[y][x]].Add(new Point2D(x, y));
                 }
             }
         }
-        
+
+        return antennae;
+    }
+
+    private static void CalculateAntinodeCount(Dictionary<char, List<Point2D>> antennae, Point2D size)
+    {
         var antinodes = new HashSet<Point2D>();
 
         foreach (var (frequency, emitters) in antennae)
@@ -33,15 +47,34 @@ public abstract class Day8
             {
                 foreach (var b in emitters.Where(b => a != b))
                 {
-                    antinodes.UnionWith(Point2D.Antinodes(a, b).Where(it => it is { X: >= 0, Y: >= 0 } && it.X < size.X && it.Y < size.Y));
+                    antinodes.UnionWith(Point2D.Antinodes(a, b)
+                        .Where(it => it is { X: >= 0, Y: >= 0 } && it.X < size.X && it.Y < size.Y));
                 }
             }
         }
+
         Console.WriteLine(antinodes.Count);
-        
-        
     }
-    
+
+    private static void CalculateResonantAntinodeCount(Dictionary<char, List<Point2D>> antennae, Point2D size)
+    {
+        var antinodes = new HashSet<Point2D>();
+
+        foreach (var (frequency, emitters) in antennae)
+        {
+            foreach (var a in emitters)
+            {
+                foreach (var b in emitters.Where(b => a != b))
+                {
+                    antinodes.UnionWith(Point2D.ResonantAntinodes(a, b, size)
+                        .Where(it => it is { X: >= 0, Y: >= 0 } && it.X < size.X && it.Y < size.Y));
+                }
+            }
+        }
+
+        Console.WriteLine(antinodes.Count);
+    }
+
     private record Point2D(int X, int Y)
     {
         public static List<Point2D> Antinodes(Point2D a, Point2D b)
@@ -52,6 +85,21 @@ public abstract class Day8
                 new Point2D(a.X - vector.X, a.Y - vector.Y),
                 new Point2D(b.X + vector.X, b.Y + vector.Y)
             ];
+        }
+
+        public static HashSet<Point2D> ResonantAntinodes(Point2D a, Point2D b, Point2D size)
+        {
+            var antinodes = new HashSet<Point2D>();
+            var vector = new Point2D(b.X - a.X, b.Y - a.Y);
+            var absoluteBounds = Math.Min(Math.Abs(size.X / vector.X), Math.Abs(size.Y / vector.Y));
+            for (int i = -absoluteBounds; i < absoluteBounds; i++)
+            {
+                antinodes.Add(
+                    new Point2D(a.X - vector.X*i, a.Y - vector.Y*i));
+                antinodes.Add(
+                    new Point2D(b.X + vector.X*i, b.Y + vector.Y*i));
+            }
+            return antinodes;
         }
     }
 }
