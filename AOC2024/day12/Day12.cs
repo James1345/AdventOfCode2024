2bbb3e94bfe715be444eaadee6256d7333f9746b
James McMahon <james1345@googlemail.com>
2024-12-12 14:05:52 +0000

    Day 12 part 2

--- AOC2024/day12/Day12.cs
+++ AOC2024/day12/Day12.cs
@@ -6,28 +6,14 @@ public class Day12
     {
         var input = File.ReadAllLines("day12/input.txt");
 
-//         input =
-// """
-// RRRRIICCFF
-// RRRRIICCCF
-// VVRRRCCFFF
-// VVRCCCJFFF
-// VVVVCJJCFE
-// VVIVCCJJEE
-// VVIIICJJEE
-// MIIIIIJJEE
-// MIIISIJEEE
-// MMMISSJEEE
-// """.Split('\n');
-        
-        
         (int w, int h) size = (input[0].Length, input.Length);
-        
+
         var set = new HashSet<(int x, int y)>();
 
         var total = 0;
+        var totalBulk = 0;
 
-        for (var y =0; y < size.h; y++)
+        for (var y = 0; y < size.h; y++)
         {
             for (var x = 0; x < size.w; x++)
             {
@@ -35,7 +21,7 @@ public class Day12
                 {
                     continue;
                 }
-                
+
                 var value = input[y][x];
                 var q = new Queue<(int x, int y)>();
                 q.Enqueue((x, y));
@@ -47,31 +33,54 @@ public class Day12
                 while (q.Count > 0)
                 {
                     var (px, py) = q.Dequeue();
-                    
-                    if(inside.Contains((px, py))) continue;
+
+                    if (inside.Contains((px, py))) continue;
 
                     if (px >= 0 && py >= 0 && px < size.w && py < size.h && input[py][px] == value)
                     {
                         area++;
                         set.Add((px, py));
                         inside.Add((px, py));
-                        
+
                         q.Enqueue((px - 1, py));
                         q.Enqueue((px + 1, py));
                         q.Enqueue((px, py - 1));
                         q.Enqueue((px, py + 1));
-                        
                     }
                     else
                     {
                         perimeter++;
                     }
                 }
-                
+
+                var corners = 0;
+                foreach (var (px, py) in inside)
+                {
+                    foreach (var corner in Corners)
+                    {
+                        if (
+                            (!inside.Contains((px + corner.x, py)) && !inside.Contains((px, py + corner.y))
+                             || (inside.Contains((px + corner.x, py)) && inside.Contains((px, py + corner.y)) &&
+                                 !inside.Contains((px + corner.x, py + corner.y)))
+                            )
+                        ) corners++;
+                    }
+                }
+
                 total += perimeter * area;
+                totalBulk += corners * area;
             }
         }
+
         Console.WriteLine(total);
+        Console.WriteLine(totalBulk);
     }
-    
+
+    private static readonly List<(int x, int y)> Corners =
+    [
+        (1, 1),
+        (1, -1),
+        (-1, -1),
+        (-1, 1),
+    ];
 }
